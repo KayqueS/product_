609e0c1c8f199d01fd2f66e6ac3be0fb20affafe
KayqueS <kayque.sanmartin65@gmail.com>
2021-03-02 18:10:49 -0300

    Add project files.

---
+++ product_/Product_data.cs
@@ -0,0 +1,29 @@
+﻿namespace product_
+{
+    class Product_data
+    {
+        public string Product;
+        public double Value;
+        public int Amount;
+
+        public double TotalValueInStock()
+        {
+            return Value * Amount;
+        }
+
+        public void AddProduct(int amount)
+        {
+            Amount += amount;
+        }
+
+        public void RemoveProduct(int amount)
+        {
+            Amount -= amount;
+        }
+
+        public override string ToString()
+        {
+            return $"{Product}, $ {Value:F2}, {Amount} unidades, Total: $ {TotalValueInStock():F2}";
+        }
+    }
+}
---
+++ product_/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace product_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product_data x = new Product_data();
+
+            Console.WriteLine("Entre com os dados do produto:");
+
+            Console.Write("Nome: ");
+            x.Product = Console.ReadLine();
+            Console.Write("Preço: ");
+            x.Value = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade em estoque: ");
+            x.Amount = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine($"Dados do produto: {x}");
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int add = int.Parse(Console.ReadLine());
+            x.AddProduct(add);
+
+
+            Console.WriteLine();
+            Console.WriteLine($"Dados atualizados: {x}");
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            int remove = int.Parse(Console.ReadLine());
+            x.RemoveProduct(remove);
+
+            Console.WriteLine();
+            Console.WriteLine($"Dados atualizados: {x}");
+            Console.WriteLine();
+        }
+    }
+}
